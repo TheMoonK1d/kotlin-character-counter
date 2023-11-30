07df9707a8a7b6e4b349832fb913cf09bd7c013e
themoonk1d <eyobdesta562@gmail.com>
2023-11-30 10:32:58 +0300

    Ex 4 character counter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 fun main() {
+
+    //Counter function
     fun count (s : String){
         val txt = s.toCharArray()
         var letter = 0
@@ -6,6 +8,10 @@ fun main() {
         var space = 0
         var other = 0
         for (inx in txt.withIndex()){
+            /*
+            * Must use value...index does not work
+            *
+            */
             if(Character.isLetter(inx.value)){
                 letter++
             }else if (Character.isDigit(inx.value)){
